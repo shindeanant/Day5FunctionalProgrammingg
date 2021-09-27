9af530b08db92de4721126531070436007eb7cf2
shindeanant <shindeanant1997@gmail.com>
2021-09-27 13:18:48 +0530

    ADD: Distance java program

--- FunctionalProgramming.java
+++ FunctionalProgramming.java
@@ -10,35 +10,15 @@ public class FunctionalProgramming {
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter the number of integer :");
+		System.out.println("Enter the values of x ");
+		int x = sc.nextInt();
 
-		int number = sc.nextInt();
+		System.out.println("Enter the value of y");
+		int y = sc.nextInt();
 
-		int[] arr = new int[number];
+		double distance = Math.sqrt(x * x + y * y);
 
-		System.out.println("Enter the integers :");
-
-		for (int i = 0; i < number; i++) {
-			arr[i] = sc.nextInt();
-		}
-
-		int distinctCount = 0;
-
-		for (int i = 0; i < number - 2; i++)
-
-			for (int j = i + 1; j < number - 1; j++)
-
-				for (int k = j + 1; k < number; k++)
-
-					if ((arr[i] + arr[j] + arr[k]) == 0) {
-
-						distinctCount++;
-
-						System.out.println("(" + arr[i] + "," + arr[j] + "," + arr[k] + ")");
-
-					}
-
-		System.out.println("Total distinct triples : " + distinctCount);
+		System.out.println("the distace is " + distance);
 
 	}
 
