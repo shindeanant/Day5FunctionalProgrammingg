7395f7a661cbb78f2b3f8e07e81373103ca8939e
shindeanant <shindeanant1997@gmail.com>
2021-09-27 13:24:35 +0530

    ADD: Find the roots of the equation

--- FunctionalProgramming.java
+++ FunctionalProgramming.java
@@ -6,20 +6,38 @@ public class FunctionalProgramming {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to functional program");
+		double secondRoot = 0, firstRoot = 0;
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter the values of x ");
-		int x = sc.nextInt();
+		System.out.println("Enter the value of a ::");
+		double a = sc.nextDouble();
 
-		System.out.println("Enter the value of y");
-		int y = sc.nextInt();
+		System.out.println("Enter the value of b ::");
+		double b = sc.nextDouble();
 
-		double distance = Math.sqrt(x * x + y * y);
+		System.out.println("Enter the value of c ::");
+		double c = sc.nextDouble();
 
-		System.out.println("the distace is " + distance);
+		double delta = (b * b) - (4 * a * c);
 
+		double sqrt = Math.sqrt(delta);
+
+		if (delta > 0) {
+
+			firstRoot = (-b + sqrt) / (2 * a);
+
+			secondRoot = (-b - sqrt) / (2 * a);
+
+			System.out.println("Roots are :: " + firstRoot + " and " + secondRoot);
+
+		} else if (delta == 0) {
+
+			System.out.println("Root is :: " + (-b + sqrt) / (2 * a));
+
+		} else {
+			System.out.println("roots ate not equal");
+		}
 	}
 
 }
