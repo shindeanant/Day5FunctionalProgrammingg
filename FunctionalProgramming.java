395b212605fe7e0983f58ed8cdf9355116ae98c0
shindeanant <shindeanant1997@gmail.com>
2021-09-27 13:07:02 +0530

    ADD: Print 2 Dimensional Array

--- FunctionalProgramming.java
+++ FunctionalProgramming.java
@@ -1,10 +1,43 @@
 package com.functionalProgrammingg;
 
+import java.util.Scanner;
+
 public class FunctionalProgramming {
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to functional program");
+
+		System.out.print("Enter 2D array size : ");
+
+		Scanner sc = new Scanner(System.in);
+
+		int rows = sc.nextInt();
+
+		int columns = sc.nextInt();
+
+		System.out.println("Enter array elements : ");
+
+		int twoD[][] = new int[rows][columns];
+
+		for (int i = 0; i < rows; i++) {
+
+			for (int j = 0; j < columns; j++) {
+
+				twoD[i][j] = sc.nextInt();
+
+			}
+		}
+
+		for (int[] x : twoD) {
+
+			for (int y : x) {
+
+				System.out.print(y + "        ");
+			}
+
+			System.out.println();
+		}
 
 	}
 
