bb82a74736a8767baff7fcf87de80939bbdfd2e1
shindeanant <shindeanant1997@gmail.com>
2021-09-27 13:03:17 +0530

    ADD: welcm message

---
+++ FunctionalProgramming.java
@@ -0,0 +1,11 @@
+package com.functionalProgrammingg;
+
+public class FunctionalProgramming {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to functional program");
+
+	}
+
+}
